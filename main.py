bf518c6746282f295e545e0889adba45728abc8e
parksaegyeol <dmsgkrkt1@naver.com>
2025-11-20 20:05:15 +0900

    refactor: Add error handle

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from typing import Union
+from typing import Union, Optional
 
 
-def add(x:Union[int, float], y:[int, float])->Union[int, float]:
-    return x+y
+def add(x:Union[int, float], y:[int, float])->Optional[Union[int, float]]:
+    try:
+        result=x+y
+    except Exception as e:
+        print(e)
+        return None
+
+    return result
 
 
 if __name__=='__main__':
@@ -10,3 +16,4 @@ if __name__=='__main__':
     print(add(1.5, 2.5))
     print(add(1, 2.5))
     print(add(1.5, 7))
+
