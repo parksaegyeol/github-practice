a2c73f7a4566f2b7a304f3182fef079483852d50
parksaegyeol <dmsgkrkt1@naver.com>
2025-11-20 17:26:31 +0900

    feat: Add add function
    #1

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+
+def add(x:Union[int, float], y:[int, float])->Union[int, float]:
+    return x+y
+
+
+if __name__=='__main__':
+    print(add(1,2))
+    print(add(1.5, 2.5))
+    print(add(1, 2.5))
+    print(add(1.5, 7))
